708ddfcf130ac574f9e5e477b1f7a421ae9b607f
MatheusKerscher <kerschermatheus12@gmail.com>
2025-10-11 09:58:15 -0300

    Project initial config

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <>
+      <h1>
+        Apenas o inicio... O Matheus do futuro irá agradecer muito ao Matheus de
+        agora
+      </h1>
+    </>
+  );
+};
+
+export default Home;

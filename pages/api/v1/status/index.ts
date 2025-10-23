a971a2a53fa0197256766288b5dcbbf88bb4740d
MatheusKerscher <kerschermatheus12@gmail.com>
2025-10-23 19:09:40 -0300

    add `database.ts` file

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,6 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default function status(req: NextApiRequest, res: NextApiResponse) {
+import database from "../../../../infra/database";
+
+export default async function status(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const result = await database.query("SELECT 1 + 1 AS sum;");
+
+  console.log(result.rows);
+
   res.status(200).json({
     message: "A princípio tudo ok",
   });

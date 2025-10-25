0313d2425e789239f40178a14f3f4c5e49657551
MatheusKerscher <kerschermatheus12@gmail.com>
2025-10-25 12:30:59 -0300

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -6,11 +6,28 @@ export default async function status(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const result = await database.query("SELECT 1 + 1 AS sum;");
+  const updateAt = new Date().toISOString();
+  const databaseVersion = (await database.query("SHOW server_version;")).rows[0]
+    .server_version;
+  const maxConnections = (await database.query("SHOW max_connections;")).rows[0]
+    .max_connections;
 
-  console.log(result.rows);
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnections = (
+    await database.query({
+      text: "SELECT COUNT(*)::int AS opened_connections FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+    })
+  ).rows[0].opened_connections;
 
   res.status(200).json({
-    message: "A princípio tudo ok",
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(maxConnections),
+        opened_connections: openedConnections,
+      },
+    },
   });
 }

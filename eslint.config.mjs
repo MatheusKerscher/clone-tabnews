b825f289d1a9a34e3a3ccb06715682137db8ce96
MatheusKerscher <kerschermatheus12@gmail.com>
2025-11-15 15:50:42 -0300

    fix `ESLint` linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript", "plugin:jest/recommended", "prettier"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:jest/recommended",
+    "prettier",
+  ),
   {
     ignores: [
       "node_modules/**",
--- test/orchestrator.ts
+++ test/orchestrator.ts
@@ -17,4 +17,6 @@ async function waitForAllServices() {
   }
 }
 
-export default { waitForAllServices };
+const orchestrator = { waitForAllServices };
+
+export default orchestrator;

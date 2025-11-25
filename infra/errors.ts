d47397143906f80b59371e2dfb5f31ef8e22b2e6
MatheusKerscher <kerschermatheus12@gmail.com>
2025-11-25 20:17:38 -0300

    feat: create and use `InternalServerError` in `/api/v1/status`

---
+++ infra/errors.ts
@@ -0,0 +1,23 @@
+export class InternalServerError extends Error {
+  action: string;
+  statusCode: number;
+
+  constructor({ cause }: { cause: any }) {
+    super("Ocorreu um erro inesperado no servidor.", {
+      cause,
+    });
+
+    this.name = "InternalServerError";
+    this.action = "Entre em contato com o suporte.";
+    this.statusCode = 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}

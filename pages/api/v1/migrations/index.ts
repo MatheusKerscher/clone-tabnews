e4a9e88017ba2dc0bd90b2c105104d7b9e1b1739
MatheusKerscher <kerschermatheus12@gmail.com>
2025-11-26 19:47:48 -0300

    feat: add `next-connect` in `/api/v1/migrations`

--- pages/api/v1/migrations/index.ts
+++ pages/api/v1/migrations/index.ts
@@ -1,58 +1,83 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
+import { createRouter } from "next-connect";
+import { InternalServerError, MethodNotAllowedError } from "infra/errors";
+
 import { runner as migrationsRunner, type RunnerOption } from "node-pg-migrate";
 import { join } from "path";
 
 import database from "infra/database";
 import { cwd } from "process";
+import type { Client } from "pg";
 
-export default async function migrations(
+const router = createRouter<NextApiRequest, NextApiResponse>();
+
+router.get(getHandler).post(postHandler);
+
+export default router.handler({
+  onNoMatch: onNoMatchHandler,
+  onError: onErrorHandler,
+});
+
+async function onNoMatchHandler(req: NextApiRequest, res: NextApiResponse) {
+  const publicErrorObject = new MethodNotAllowedError();
+  res.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+async function onErrorHandler(
+  error: any,
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const allowedMethods = ["GET", "POST"];
-  const currentMethod = req.method!;
+  const publicErrorObject = new InternalServerError({ cause: error });
 
-  if (!allowedMethods.includes(currentMethod)) {
-    return res.status(405).json({
-      message: `Method ${currentMethod} not allowed to this request!`,
-    });
-  }
+  console.log("\n Erro no next-connect:");
+  console.error(publicErrorObject);
 
+  res.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
+
+async function getHandler(req: NextApiRequest, res: NextApiResponse) {
   let client;
 
   try {
     client = await database.getNewClient();
+    const migrationOptions = getRunnerOptions(client, true);
+    const pendingMigrations = await migrationsRunner(migrationOptions);
+    res.status(200).json(pendingMigrations);
+  } finally {
+    await client?.end();
+  }
+}
 
-    const defaultMigrationOptions: RunnerOption = {
-      dbClient: client,
-      dryRun: true,
-      dir: join(cwd(), "infra", "migrations"),
-      direction: "up",
-      migrationsTable: "pgmigrations",
-      verbose: true,
-    };
+async function postHandler(req: NextApiRequest, res: NextApiResponse) {
+  let client;
 
-    if (currentMethod === "GET") {
-      const pendingMigrations = await migrationsRunner(defaultMigrationOptions);
-
-      return res.status(200).json(pendingMigrations);
-    }
-
-    const migratedMigrations = await migrationsRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-    });
+  try {
+    client = await database.getNewClient();
+    const migrationOptions = getRunnerOptions(client, false);
+    const migratedMigrations = await migrationsRunner(migrationOptions);
 
     if (migratedMigrations.length) {
       return res.status(201).json(migratedMigrations);
     }
 
-    return res.status(200).json(migratedMigrations);
-  } catch (error) {
-    console.log(error);
-    throw error;
+    res.status(200).json(migratedMigrations);
   } finally {
     await client?.end();
   }
+}
+
+function getRunnerOptions(client: Client, isDryRun: boolean) {
+  const migrationOptions: RunnerOption = {
+    dbClient: client,
+    dryRun: isDryRun,
+    dir: join(cwd(), "infra", "migrations"),
+    direction: "up",
+    migrationsTable: "pgmigrations",
+    verbose: true,
+  };
+
+  return migrationOptions;
 }

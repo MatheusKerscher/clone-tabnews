8496166a462a4259d286a365c3bbbe484a24b7e1
MatheusKerscher <kerschermatheus12@gmail.com>
2025-11-27 19:22:58 -0300

    refactor: make `/api/v1/migrations` use `migrator` model

--- pages/api/v1/migrations/index.ts
+++ pages/api/v1/migrations/index.ts
@@ -1,61 +1,29 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import { join } from "path";
-import { cwd } from "process";
-
 import { createRouter } from "next-connect";
-import type { Client } from "pg";
-import { runner as migrationsRunner, type RunnerOption } from "node-pg-migrate";
 
 import controller from "infra/controller";
-import database from "infra/database";
+import migrator from "models/migrator";
 
 const router = createRouter<NextApiRequest, NextApiResponse>();
 
-router.get(getHandler).post(postHandler);
+router.get(getHandler);
+router.post(postHandler);
 
 export default router.handler(controller.errorHandler);
 
 async function getHandler(req: NextApiRequest, res: NextApiResponse) {
-  let client;
+  const pendingMigrations = await migrator.listPendingMigrations();
 
-  try {
-    client = await database.getNewClient();
-    const migrationOptions = getRunnerOptions(client, true);
-    const pendingMigrations = await migrationsRunner(migrationOptions);
-    res.status(200).json(pendingMigrations);
-  } finally {
-    await client?.end();
-  }
+  res.status(200).json(pendingMigrations);
 }
 
 async function postHandler(req: NextApiRequest, res: NextApiResponse) {
-  let client;
+  const migratedMigrations = await migrator.runPendingMigrations();
 
-  try {
-    client = await database.getNewClient();
-    const migrationOptions = getRunnerOptions(client, false);
-    const migratedMigrations = await migrationsRunner(migrationOptions);
-
-    if (migratedMigrations.length) {
-      return res.status(201).json(migratedMigrations);
-    }
-
-    res.status(200).json(migratedMigrations);
-  } finally {
-    await client?.end();
+  if (migratedMigrations.length) {
+    return res.status(201).json(migratedMigrations);
   }
-}
 
-function getRunnerOptions(client: Client, isDryRun: boolean) {
-  const migrationOptions: RunnerOption = {
-    dbClient: client,
-    dryRun: isDryRun,
-    dir: join(cwd(), "infra", "migrations"),
-    direction: "up",
-    migrationsTable: "pgmigrations",
-    verbose: true,
-  };
-
-  return migrationOptions;
+  res.status(200).json(migratedMigrations);
 }

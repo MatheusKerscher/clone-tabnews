4a6ba4bca089398c222efb35c14476b1db609ea0
MatheusKerscher <kerschermatheus12@gmail.com>
2025-11-29 19:41:35 -0300

    feat: create `users` migration

---
+++ infra/migrations/1764452467633_create-users.mjs
@@ -0,0 +1,45 @@
+export const up = (pgm) => {
+  pgm.createTable("users", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()")
+    },
+
+    username: {
+      type: "varchar(30)",
+      notNull: true,
+      unique: true
+    },
+
+    // Why 254 characters? https://stackoverflow.com/a/1199238
+    email: {
+      type: "varchar(254)",
+      notNull: true,
+      unique: true
+    },
+
+    // Why 60 characters? https://www.npmjs.com/package/bcrypt#hash-info
+    password: {
+      type: "varchar(60)",
+      notNull: true,
+      unique: true
+    },
+
+    // Why timestamp with timezone? https://justatheory.com/2012/04/postgres-use-timestamptz/
+    created_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', NOW())")
+    },
+
+    updated_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', NOW())")
+    }
+  })
+};
+
+
+export const down = false

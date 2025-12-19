e5113d504a0485622b1b18edfd4b087a9839ae5d
MatheusKerscher <kerschermatheus12@gmail.com>
2025-12-19 14:14:52 -0300

    feat: add default feature `read:activation_token` when creating `user`

---
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -0,0 +1,51 @@
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+  await orchestrator.deleteAllEmails()
+});
+
+
+describe("Use case: Registration Flow (all successful)", () => {
+  test("Create user account", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: "FlowRegistration",
+        email: "flow.registration@email.com",
+        password: "senha123",
+      }),
+    });
+    expect(response.status).toBe(201);
+
+    const responseBody = await response.json();
+
+    expect(responseBody).toEqual({
+      id: responseBody.id,
+      username: "FlowRegistration",
+      email: "flow.registration@email.com",
+      features: ["read:activation_token"],
+      password: responseBody.password,
+      created_at: responseBody.created_at,
+      updated_at: responseBody.updated_at,
+    });
+  });
+
+  test("Receive activation email", async () => {
+  });
+
+  test("Activate account", async () => {
+  });
+
+  test("Login", async () => {
+  });
+
+  test("Get user information", async () => {
+  });
+});
+

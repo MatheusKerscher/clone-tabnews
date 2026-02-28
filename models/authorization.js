1ca8b3cd7b82743f9e9453ef6c03d060a66efd4a
MatheusKerscher <kerschermatheus12@gmail.com>
2026-02-28 15:01:05 -0300

    feat: validate `user`, `feature` and `resource` in `authorization` model

--- models/authorization.js
+++ models/authorization.js
@@ -1,4 +1,33 @@
+import { InternalServerError } from "infra/errors"
+
+const availableFeatures = [
+  // USER
+  "create:user",
+  "read:user",
+  "read:user:self",
+  "update:user",
+  "update:user:others",
+
+  // SESSION
+  "create:session",
+  "read:session",
+
+  // ACTIVATION_TOKEN
+  "read:activation_token",
+
+  // MIGRATION
+  "create:migration",
+  "read:migration",
+
+  //STATUS
+  "read:status",
+  "read:status:all"
+]
+
 function can(user, feature, resource) {
+  validateUser(user)
+  validateFeature(feature)
+
   let authorized = false
 
   if (user.features.includes(feature)) {
@@ -17,6 +46,10 @@ function can(user, feature, resource) {
 }
 
 function filterOutput(user, feature, resource) {
+  validateUser(user)
+  validateFeature(feature)
+  validateResource(resource)
+
   if (feature === "read:user") {
     return {
       id: resource.id,
@@ -99,3 +132,27 @@ const authorization = {
 }
 
 export default authorization
+
+function validateUser(user) {
+  if (!user || !user.features) {
+    throw new InternalServerError({
+      cause: "É necessário informar um 'user' no model 'authorization'"
+    })
+  }
+}
+
+function validateFeature(feature) {
+  if (!feature || !availableFeatures.includes(feature)) {
+    throw new InternalServerError({
+      cause: "É necessário informar uma 'feature' no model 'authorization'"
+    })
+  }
+}
+
+function validateResource(resource) {
+  if (!resource) {
+    throw new InternalServerError({
+      cause: "É necessário informar um 'resource' no model 'authorization.filterOutput()'"
+    })
+  }
+}
---
+++ tests/unit/models/authorization.test.js
@@ -0,0 +1,104 @@
+const { InternalServerError } = require("infra/errors")
+const { default: authorization } = require("models/authorization")
+
+describe("models/authorization.js", () => {
+  describe(".can()", () => {
+    test("Without 'user'", () => {
+      expect(() => {
+        authorization.can()
+      }).toThrow(InternalServerError)
+    })
+
+    test("Without 'user.features'", () => {
+      const createUser = {
+        username: "semFeatures"
+      }
+
+      expect(() => {
+        authorization.can(createUser)
+      }).toThrow(InternalServerError)
+    })
+
+    test("Without unknown 'feature'", () => {
+      const createUser = {
+        features: ["read:user"]
+      }
+
+      expect(() => {
+        authorization.can(createUser, "unknown:object")
+      }).toThrow(InternalServerError)
+    })
+
+    test("With valid 'user' and known 'feature'", () => {
+      const createUser = {
+        features: ["read:user"]
+      }
+
+      expect(authorization.can(createUser, "read:user")).toBe(true)
+    })
+  })
+
+  describe(".filterOutput()", () => {
+    test("Without 'user'", () => {
+      expect(() => {
+        authorization.filterOutput()
+      }).toThrow(InternalServerError)
+    })
+
+    test("Without 'user.features'", () => {
+      const createUser = {
+        username: "semFeatures"
+      }
+
+      expect(() => {
+        authorization.filterOutput(createUser)
+      }).toThrow(InternalServerError)
+    })
+
+    test("With unknown 'feature'", () => {
+      const createUser = {
+        features: ["read:user"]
+      }
+
+      expect(() => {
+        authorization.filterOutput(createUser, "unknown:object")
+      }).toThrow(InternalServerError)
+    })
+
+    test("With valid 'user', known 'feature' but no 'resource'", () => {
+      const createUser = {
+        features: ["read:user"]
+      }
+
+      expect(() => {
+        authorization.filterOutput(createUser, "read:user")
+      }).toThrow(InternalServerError)
+    })
+
+    test("With valid 'user', known 'feature' and 'resource'", () => {
+      const createUser = {
+        features: ["read:user"]
+      }
+
+      const resource = {
+        id: 1,
+        username: "resource",
+        features: ["create:user"],
+        created_at: "2026-01-01T00:00:00.000Z",
+        updated_at: "2026-01-01T00:00:00.000Z",
+        email: "resource@email.com",
+        password: "resource1234"
+      }
+
+      const result = authorization.filterOutput(createUser, "read:user", resource)
+
+      expect(result).toEqual({
+        id: 1,
+        username: "resource",
+        features: ["create:user"],
+        created_at: "2026-01-01T00:00:00.000Z",
+        updated_at: "2026-01-01T00:00:00.000Z"
+      })
+    })
+  })
+})

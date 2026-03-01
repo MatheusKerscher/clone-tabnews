87590cd841e30486ea4cfaf6411822ba06e151a1
MatheusKerscher <kerschermatheus12@gmail.com>
2026-03-01 09:48:59 -0300

    refactor: improve loggin to `email.send()`

--- infra/errors.js
+++ infra/errors.js
@@ -26,14 +26,15 @@ export class ServiceError extends Error {
   action;
   statusCode;
 
-  constructor({ cause, message }) {
+  constructor({ cause, message, action, context }) {
     super(message || "Serviço indisponível no momento.", {
       cause,
     });
 
     this.name = "ServiceError";
-    this.action = "Verifique se o serviço está disponível.";
+    this.action = action || "Verifique se o serviço está disponível.";
     this.statusCode = 503;
+    this.context = context
   }
 
   toJSON() {
@@ -42,6 +43,7 @@ export class ServiceError extends Error {
       message: this.message,
       action: this.action,
       status_code: this.statusCode,
+      context: this.context
     };
   }
 }
